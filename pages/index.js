48da13df00711c05587734ca083de6d608d19a00
almoral323 <almoral323@gmail.com>
2023-03-30 22:05:09 -0400

    nav, profile, services updated

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
-
-
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import {AiFillLinkedin, AiFillGithub, GiSnakeSpiral} from 'react-icons/ai';
+import Image from 'next/image';
+import kanye from '../public/kanye.png'
+import design from '../public/design.jpg'
+import consulting from '../public/consulting.jpg'
+import coding from '../public/coding.jpg'
 
 export default function Home() {
+  
   return (
     <div>
       <Head>
@@ -12,7 +18,59 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main className='bg-white px-10'>
+        <section className="min-h-screen">
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl'>developed by almoral323</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className='cursor-pointer text-2xl' />
+              </li>
+              <li>
+                <a className=' bg-gradient-to-r  from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a>
+              </li>
+            </ul>
+          </nav>
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-500 font-medium'>Alfredo Morales</h2>
+            <h3 className='text-2xl py-2'>Developer and Designer</h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>Description HERE</p>
+          </div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+            <AiFillLinkedin />
+            <AiFillGithub />
+          </div>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+            <Image src={kanye} fill="fill" />
+          </div>
+        </section>
+        
+        <section>
+          <div className='text-center'>
+            <h3 className='text-3xl py-1'>Services I offer</h3>
+            <p className='text-md py-2 leading-8 text-gray-800'> Description of what I've done in the past and my journey. Mention project ive completed to businesses and people.</p>
+            <p className='text-md py-2 leading-8 text-gray-800'> A continued description with some more text here.</p>
+          </div>
+
+          <div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src= {design} width={200} height={100}/>
+              <h3 className='text-lg font-medium pt-8 pb-2'>
+                Attractive Designs
+              </h3>
+
+              <p className='py-2'>
+                Developing elegant and professional desgins that suit you needs and vision.
+              </p>
+
+              <h4 className='py-4 text-teal-600'>Design Tools</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p>Illustrator</p>
+            </div>
+          </div>
+        </section>
+
+      </main>
     </div>
   );
 }
